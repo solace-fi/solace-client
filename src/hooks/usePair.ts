34290e1f84e644a1d2549a2dc0ecd8bead9ff85d
Danny Fung <dannyfung97@gmail.com>
2021-05-11 14:59:22 -0700

    fix<usePair>: SOLACE-WETH pair

--- src/hooks/usePair.ts
+++ src/hooks/usePair.ts
@@ -1,28 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ChainId, Fetcher, Route, WETH, Trade, TokenAmount, TradeType } from '@uniswap/sdk'
 
-const chainId = ChainId.MAINNET
-const tokenAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // tokenaddress of DAI
+const chainId = ChainId.RINKEBY
+const tokenAddress = '0x44B843794416911630e74bAB05021458122c40A0' // rinkeby tokenaddress of SOLACE
 
 export function usePairPrice(): any {
-  const [pairPrice, setPairPrice] = useState<any>()
+  const [pairPrice, setPairPrice] = useState<any>('-')
 
   useEffect(() => {
     const getPairPrice = async () => {
-      // Fetch Token Data according to TokenAddress taken from etherscan
-      const dai = await Fetcher.fetchTokenData(chainId, tokenAddress)
+      try {
+        // Fetch Token Data according to TokenAddress taken from etherscan
+        const solace = await Fetcher.fetchTokenData(chainId, tokenAddress)
 
-      // Fetch Trading Token Data ' Wrapped Ether '
-      const weth = WETH[chainId]
+        // Fetch Trading Token Data ' Wrapped Ether '
+        const weth = WETH[chainId]
 
-      // Fetch theoretical pair data with dai and weth
-      const pair = await Fetcher.fetchPairData(dai, weth)
-      const route = new Route([pair], weth)
+        // Fetch theoretical pair data with solace and weth
+        const pair = await Fetcher.fetchPairData(solace, weth)
+        const route = new Route([pair], weth)
 
-      // Fetch theoretical prices when trading 1 wETH to DAI
-      const trade = new Trade(route, new TokenAmount(weth, '1000000000000000'), TradeType.EXACT_INPUT)
-      const pairPrice = trade.executionPrice.toSignificant(6)
-      setPairPrice(pairPrice)
+        // Fetch theoretical prices when trading 1 wETH to SOLACE
+        const trade = new Trade(route, new TokenAmount(weth, '1000000000000000'), TradeType.EXACT_INPUT)
+        const pairPrice = trade.executionPrice.toSignificant(6)
+        setPairPrice(pairPrice)
+      } catch (err) {
+        console.log('getPairPrice', err)
+      }
     }
     getPairPrice()
   }, [])
