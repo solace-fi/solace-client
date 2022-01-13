42a808e75746100e2b9af04214bbf4e549f85410
dredshep <dredshep@gmail.com>
2022-01-13 15:57:01 -0400

    dunno what i've changed b ut we'll check soonish

--- src/pages/stake/components/CardRange.tsx
+++ src/pages/stake/components/CardRange.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { DefaultTheme, ThemedStyledFunction } from 'styled-components'
+import styled from 'styled-components'
 
 const StyledRangeInput = styled.input`
   &::-webkit-range-thumb,
