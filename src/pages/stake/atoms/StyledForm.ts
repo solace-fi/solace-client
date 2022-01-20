9cda98578e9dca5d8087e196b37072e0ce1733c8
Danny Fung <dannyfung97@gmail.com>
2022-01-20 08:53:03 -0800

    fix: styled.form is now styled.div

--- src/pages/stake/atoms/StyledForm.ts
+++ src/pages/stake/atoms/StyledForm.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { BKPT_5 } from '../../../constants'
 
-export const StyledForm = styled.form`
+export const StyledForm = styled.div`
   display: flex;
   flex-direction: column;
   align-items: start;
