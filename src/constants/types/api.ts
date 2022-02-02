2deaa551ee04b34ddbcbe95466528b98f4e20a41
Danny Fung <dannyfung97@gmail.com>
2022-02-02 15:06:29 -0800

    fix: soteria apil calls

--- src/constants/types/api.ts
+++ src/constants/types/api.ts
@@ -2,5 +2,36 @@ export type SolaceRiskBalance = {
   network: string
   appId: string
   balanceUSD: number
-  balance: number
+  balanceETH: number
+}
+
+export type SolaceRiskScore = {
+  address: string
+  address_rp: number
+  current_rate: number
+  timestamp: string
+  metadata: {
+    seriesName: string
+    version: string
+    dateCreated: string
+    provenance: string
+    generatedBy: string
+    sourceDataType: string
+    sourceDataVersion: string
+  }
+  protocols: SolaceRiskProtocol[]
+}
+
+export type SolaceRiskProtocol = {
+  appId: string
+  balanceETH: number
+  balanceUSD: number
+  category: string
+  network: string
+  riskLoad: number
+  rol: number
+  rrol: number
+  tier: number
+  ['rp-usd']: number
+  ['risk-adj']: number
 }
