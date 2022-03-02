a8e0911fd38a933e216814deec9ffb5e26ba8fb9
dredshep <dredshep@gmail.com>
2022-03-02 17:31:07 -0400

    feat: homepage scrollbar no longer visible on firefox

--- src/pages/about/components/organisms/DesktopScrollableArea.tsx
+++ src/pages/about/components/organisms/DesktopScrollableArea.tsx
@@ -8,6 +8,7 @@ const SnapScroll = styled.div`
   scroll-snap-type: y mandatory;
   overflow-y: scroll;
   scroll-behavior: smooth;
+  scrollbar-width: none;
   & > * {
     scroll-snap-align: start;
     height: calc(100vh - 100px);
