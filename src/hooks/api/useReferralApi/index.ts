fbd008f84829992b973cb412da78d66d0a1c272f
Danny Fung <dannyfung97@gmail.com>
2022-07-29 10:03:59 -0700

    feat: referral endpoints in SDK

--- src/hooks/api/useReferralApi/index.ts
+++ src/hooks/api/useReferralApi/index.ts
@@ -1,11 +1,9 @@
+import { BigNumber, PolicyReferral } from '@solace-fi/sdk-nightly'
 import { useWeb3React } from '@web3-react/core'
-import { useState, useEffect, useCallback } from 'react'
-import { useLocalStorage } from 'react-use-storage'
+import { useState, useEffect, useCallback, useMemo } from 'react'
 import { useGeneral } from '../../../context/GeneralManager'
 import { useNetwork } from '../../../context/NetworkManager'
 import { useCheckIsCoverageActive } from '../../policy/useSolaceCoverProductV3'
-import { GetByUserResponse } from './GetByUserResponse'
-import { InfoResponse, InfoResponseArray, AppliedPromoCode } from './InfoResponse'
 
 export default function useReferralApi(): {
   userReferralCode: string | undefined
@@ -22,6 +20,8 @@ export default function useReferralApi(): {
   const { activeNetwork } = useNetwork()
   const { policyId } = useCheckIsCoverageActive()
 
+  const policyReferralObj = useMemo(() => new PolicyReferral(), [])
+
   const [userReferralCode, setUserReferralCode] = useState<string | undefined>(undefined)
   const [earnedAmount, setEarnedAmount] = useState<number | undefined>(undefined)
   const [referredCount, setReferredCount] = useState<number | undefined>(undefined)
@@ -29,73 +29,23 @@ export default function useReferralApi(): {
   const [cookieCode, setCookieCode] = useState<string | undefined>(undefined)
   const [cookieCodeUsable, setCookieCodeUsable] = useState<boolean>(false)
 
-  const baseApiUrl = 'https://2vo3wfced8.execute-api.us-west-2.amazonaws.com/prod/'
-
-  const getUserReferralCode = useCallback(async () => {
-    if (!account || !policyId || policyId.isZero()) return
-    // if (!account /**|| !policyId || policyId.isZero() */) return
-    const getUserReferralCodeUrl = `${baseApiUrl}referral-codes?user=${account}`
-    const response = await fetch(getUserReferralCodeUrl)
-    const data = (await response.json()) as GetByUserResponse
-    const _referralCode = data.result?.[0]?.referral_code
-
-    // if there is no referral code, then we must set it by calling baseUrl + referral-codes
-    // on success, we will have either Message or `result.referral_code`
-    if (!_referralCode) {
-      const postReferralCodeUrl = `${baseApiUrl}referral-codes`
-      const postReferralCode = async () => {
-        const response = await fetch(postReferralCodeUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': process.env.REACT_APP_REFERRAL_API_KEY as string,
-          },
-          body: JSON.stringify({
-            user: account,
-            // chain_id: 4,
-            // policy_id: 12,
-            chain_id: activeNetwork.chainId,
-            policy_id: policyId.toNumber(),
-          }),
-        })
-        const data = (await response.json()) as InfoResponse
-        const _referralCode = data.result?.referral_codes?.[0]?.referral_code
-        _referralCode && setUserReferralCode(_referralCode)
-      }
-      postReferralCode()
-    } else setUserReferralCode(_referralCode)
-  }, [account, activeNetwork, policyId])
-
   const getInfo = useCallback(async () => {
-    if (!account) return
-    const getInfoUrl = `${baseApiUrl}info?user=${account}`
-    const response = await fetch(getInfoUrl, {
-      method: 'GET',
-      headers: {
-        'X-API-KEY': process.env.REACT_APP_REFERRAL_API_KEY as string,
-      },
-    })
-    const data = (await response.json()) as InfoResponse
-    const _earnedAmount = data.result?.reward_accounting?.referred_earns
-    const _referredCount = !userReferralCode ? 0 : data.result?.referred_users.length
-    const _appliedCode = data.result?.applied_referral_codes?.[0]?.referral_code
-    _earnedAmount ? setEarnedAmount(_earnedAmount) : setEarnedAmount(0)
-    _referredCount ? setReferredCount(_referredCount) : setReferredCount(0)
-    _appliedCode ? setAppliedCode(_appliedCode) : setAppliedCode('')
-  }, [account, userReferralCode])
+    if (!account || !policyId || policyId.isZero()) return
+    const res = await policyReferralObj.getInfo(account, policyId, activeNetwork.chainId)
+    setEarnedAmount(res.earnedAmount)
+    setReferredCount(res.referredCount)
+    setAppliedCode(res.appliedCode)
+    setUserReferralCode(res.referralCode)
+    console.log('referral - info', res)
+  }, [account, activeNetwork, policyId, policyReferralObj])
 
-  const checkReferralCodeUsability = useCallback(async (referral_code: string) => {
-    const url = `${baseApiUrl}referral-codes?referral_code=${referral_code}`
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'X-API-KEY': process.env.REACT_APP_REFERRAL_API_KEY as string,
-      },
-    })
-    const data = (await response.json()) as InfoResponseArray
-    const _referralCode = data.result?.length > 0
-    setCookieCodeUsable(_referralCode)
-  }, [])
+  const checkReferralCodeUsability = useCallback(
+    async (referral_code: string) => {
+      const canBeUsed = await policyReferralObj.isReferralCodeUsable(referral_code)
+      setCookieCodeUsable(canBeUsed)
+    },
+    [policyReferralObj]
+  )
 
   useEffect(() => {
     const code = localStorageReferralCode
@@ -107,49 +57,32 @@ export default function useReferralApi(): {
     }
 
     setTimeout(() => {
-      // GET OWN CODE
-      getUserReferralCode()
-
-      // GET EARNED AMOUNT, REFERRED COUNT and APPLIED CODE
       getInfo()
     }, 400)
-  }, [getInfo, getUserReferralCode, account, activeNetwork, policyId, localStorageReferralCode])
+  }, [getInfo, account, activeNetwork, policyId, localStorageReferralCode])
 
   useEffect(() => {
     if (cookieCode) checkReferralCodeUsability(cookieCode)
   }, [cookieCode, checkReferralCodeUsability])
 
-  const applyCode = async (referral_code: string, policy_id: number, chain_id: number) => {
-    console.log('referral - applying code', referral_code)
-    console.log('referral - policy_id', policy_id)
-    console.log('referral - chain_id', chain_id)
-    if (!account) return false
-    console.log('referral - account found, policyId found')
-    if (appliedCode) return false
-    // add: promo-codes/apply
-    const applyCodeUrl = `${baseApiUrl}referral-codes/apply`
-    const response = await fetch(applyCodeUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': process.env.REACT_APP_REFERRAL_API_KEY as string,
-      },
-      body: JSON.stringify({
-        user: account,
-        // chain_id: 4,
-        // policy_id: 1,
-        chain_id,
-        policy_id,
-        referral_code,
-      }),
-    })
-    const data = await response.json()
-    console.log('referral - api response', data)
-    const _appliedCode = data.result?.referral_code
-    _appliedCode && setAppliedCode(_appliedCode)
-    console.log('referral - applied code', _appliedCode)
-    return _appliedCode ? true : false
-  }
+  const applyCode = useCallback(
+    async (referral_code: string, policy_id: number, chain_id: number) => {
+      console.log('referral - applying code', referral_code)
+      console.log('referral - policy_id', policy_id)
+      console.log('referral - chain_id', chain_id)
+      if (!account) return false
+      console.log('referral - account found, policyId found')
+      if (appliedCode) return false
+      const res = await policyReferralObj.applyCode(account, referral_code, BigNumber.from(policy_id), chain_id)
+      console.log('referral - api response', res)
+      if (res.message !== 'not found' && res.message !== 'failure' && res.status) {
+        setAppliedCode(res.message)
+        return true
+      }
+      return false
+    },
+    [account, appliedCode, policyReferralObj]
+  )
 
   return {
     userReferralCode,
