c8a2cf5eb98c25e71ee36ba763ce83f683b1beb9
Danny Fung <dannyfung97@gmail.com>
2022-08-22 13:39:47 -0700

    fix: made terms and cond modal less strict

--- src/components/organisms/TermsModal.tsx
+++ src/components/organisms/TermsModal.tsx
@@ -1,36 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useGeneral } from '../../context/GeneralManager'
 import { Button } from '../atoms/Button'
-import { StyledLinkExternal } from '../atoms/Icon'
 import { Flex } from '../atoms/Layout/Page'
 import { HyperLink } from '../atoms/Link'
 import { Text } from '../atoms/Typography'
 import { Modal } from '../molecules/Modal'
 
-export const TermsModal = ({ show, handleClose }: { show: boolean; handleClose: () => void }) => {
+export const TermsModal = ({ show, handleClose }: { show: boolean; handleClose: () => void }): JSX.Element => {
   const { appTheme, acceptTerms } = useGeneral()
-  const [read, setRead] = useState(false)
-  const [nextStep, setNextStep] = useState(false)
 
   const handleCloseModal = () => {
     acceptTerms()
     handleClose()
   }
 
-  useEffect(() => {
-    if (!read) return
-    const interval = setInterval(() => {
-      setNextStep(true)
-    }, 3000)
-    return () => clearInterval(interval)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [read])
-
   return (
     <Modal isOpen={show} handleClose={handleClose} modalTitle={'Terms & Conditions'} disableCloseButton>
       <Flex col itemsCenter gap={20}>
-        <Text textAlignCenter width={230}>
-          Please read through our Terms and Conditions to use this website.
+        <Text textAlignCenter width={270}>
+          By entering this website, you agree to the terms and conditions.
         </Text>
         <HyperLink
           href={'https://www.solace.fi/solace-terms-and-conditions'}
@@ -45,16 +33,13 @@ export const TermsModal = ({ show, handleClose }: { show: boolean; handleClose: 
             techygradient={appTheme == 'light'}
             secondary
             noborder
-            onClick={() => setRead(true)}
           >
-            <Text t1>
-              Read <StyledLinkExternal size={26} />
-            </Text>
+            <Text t1>Read</Text>
           </Button>
         </HyperLink>
-        <Button info py={10} widthP={100} disabled={!nextStep} onClick={handleCloseModal}>
+        <Button info py={10} widthP={100} onClick={handleCloseModal}>
           <Text t2_5s lineHeight={20}>
-            I have read and agree to the terms and conditions
+            Continue onto website
           </Text>
         </Button>
       </Flex>
